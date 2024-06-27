c3046317debd02cd3b29a8ab8a8f901d8b287c47
Matéo <mateo.escalas@oppus.fr>
2024-06-27 12:31:48 +0200

    animated list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,212 @@
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { motion, AnimatePresence, LayoutGroup, Reorder } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { Card, CardContent } from "./components/ui/card";
+import { Repeat, XIcon } from "lucide-react";
+import { Checkbox } from "./components/ui/checkbox";
+import clsx from "clsx";
+
+const ITEMS = [
+  {
+    text: "Finish blog post ✍️",
+    checked: false,
+    id: 1,
+  },
+  {
+    text: "Build new Three.js experiences ✨",
+    checked: false,
+    id: 2,
+  },
+  {
+    text: "Add new components to Design System 🌈",
+    checked: false,
+    id: 3,
+  },
+  {
+    text: "Make some coffee ☕️",
+    checked: false,
+    id: 4,
+  },
+  {
+    text: "Drink water 💧",
+    checked: false,
+    id: 5,
+  },
+  {
+    text: "Go to the gym 🏃‍♂️",
+    checked: false,
+    id: 6,
+  },
+];
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [items, setItems] = useState(ITEMS);
+  const [height, setHeight] = useState(0);
+  const elementRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        const rect = entry.target.getBoundingClientRect();
+
+        setHeight(rect.height);
+      }
+    });
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
+
+  const completeItem = (id: number) => {
+    const updatedItems = items.map((item) => {
+      if (item.id === id) {
+        const updatedItem = {
+          ...item,
+          checked: !item.checked,
+        };
+
+        return updatedItem;
+      }
+
+      return item;
+    });
+
+    setItems(updatedItems);
+  };
+
+  return (
+    <TooltipProvider>
+      <Card className="w-[640px] mx-auto mt-[10rem]">
+        <CardContent
+          as={motion.div}
+          animate={{ height }}
+          transition={{ duration: 0.3 }}
+        >
+          <div
+            ref={elementRef}
+            className="flex flex-col gap-4 items-start px-0 py-6"
+          >
+            <div className="flex gap-4">
+              <Button
+                disabled={items.length > 5}
+                onClick={() =>
+                  setItems((prev) => {
+                    return [
+                      ...prev,
+                      {
+                        text: "Prepare for a space travel 🚀",
+                        id: Math.random(),
+                        checked: false,
+                      },
+                    ];
+                  })
+                }
+              >
+                Add item
+              </Button>
+              <Tooltip key="tooltip-reset-list">
+                <TooltipTrigger>
+                  <Button onClick={() => setItems(ITEMS)}>
+                    <Repeat className="h-4 w-4" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Reset task list</p>
+                </TooltipContent>
+              </Tooltip>
+            </div>
+            <LayoutGroup>
+              <Reorder.Group
+                axis="y"
+                values={items}
+                onReorder={setItems}
+                className="flex flex-col gap-3 m-0 p-0 w-full"
+              >
+                <AnimatePresence>
+                  {items.map((item) => (
+                    <motion.div
+                      key={item.id}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{
+                        opacity: 0,
+                        transition: {
+                          duration: 0.2,
+                        },
+                      }}
+                      transition={{
+                        delay: 0.2,
+                      }}
+                      className="flex items-center justify-between gap-6"
+                    >
+                      <Card
+                        as={Reorder.Item}
+                        value={item}
+                        className="h-full grow list-none cursor-grab"
+                        style={{
+                          position: "relative",
+                          borderRadius: "12px",
+                          width: item.checked ? "70%" : "100%",
+                        }}
+                      >
+                        <CardContent
+                          as={motion.div}
+                          layout="position"
+                          className="flex items-center gap-4 p-4"
+                        >
+                          <Checkbox
+                            id={`checkbox-${item.id}`}
+                            aria-label="Mark as done"
+                            checked={item.checked}
+                            onClick={() => completeItem(item.id)}
+                          />
+                          <p className="mb-[2px]">{item.text}</p>
+                        </CardContent>
+                      </Card>
+                      <AnimatePresence initial={false}>
+                        {item.checked ? (
+                          <motion.div
+                            layout
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1, transition: { delay: 0.2 } }}
+                            exit={{ opacity: 0 }}
+                          >
+                            <Button
+                              variant="secondary"
+                              size="icon"
+                              onClick={() =>
+                                setItems((prev) =>
+                                  prev.filter((task) => task.id !== item.id)
+                                )
+                              }
+                            >
+                              <XIcon />
+                            </Button>
+                          </motion.div>
+                        ) : null}
+                      </AnimatePresence>
+                    </motion.div>
+                  ))}
+                </AnimatePresence>
+              </Reorder.Group>
+              <motion.div layout>
+                <p>Check items off the list when you&apos;re done!</p>
+              </motion.div>
+            </LayoutGroup>
+          </div>
+        </CardContent>
+      </Card>
+    </TooltipProvider>
+  );
 }
 
 export default App;
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,21 +1,46 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
-      className
-    )}
-    {...props}
-  />
-))
-Card.displayName = "Card"
+type AsProp<C extends React.ElementType> = {
+  as?: C;
+};
+
+type CardProps<C extends React.ElementType> = React.PropsWithChildren<
+  AsProp<C>
+> & {
+  className?: string;
+  ref?: React.ComponentPropsWithRef<C>["ref"];
+} & Omit<React.ComponentPropsWithoutRef<C>, "as" | "className" | "ref">;
+
+type CardComponent = (<C extends React.ElementType = "div">(
+  props: CardProps<C>
+) => React.ReactElement) & {
+  displayName?: string;
+};
+
+const Card: CardComponent = React.forwardRef(
+  <C extends React.ElementType = "div">(
+    { as, className, children, ...props }: CardProps<C>,
+    ref: React.ComponentPropsWithRef<C>["ref"]
+  ) => {
+    const Component = as || "div";
+    return (
+      <Component
+        ref={ref}
+        className={cn(
+          "rounded-xl border bg-card text-card-foreground shadow",
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </Component>
+    );
+  }
+) as CardComponent;
+
+Card.displayName = "Card";
 
 const CardHeader = React.forwardRef<
   HTMLDivElement,
@@ -26,8 +51,8 @@ const CardHeader = React.forwardRef<
     className={cn("flex flex-col space-y-1.5 p-6", className)}
     {...props}
   />
-))
-CardHeader.displayName = "CardHeader"
+));
+CardHeader.displayName = "CardHeader";
 
 const CardTitle = React.forwardRef<
   HTMLParagraphElement,
@@ -38,8 +63,8 @@ const CardTitle = React.forwardRef<
     className={cn("font-semibold leading-none tracking-tight", className)}
     {...props}
   />
-))
-CardTitle.displayName = "CardTitle"
+));
+CardTitle.displayName = "CardTitle";
 
 const CardDescription = React.forwardRef<
   HTMLParagraphElement,
@@ -50,16 +75,37 @@ const CardDescription = React.forwardRef<
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
-))
-CardDescription.displayName = "CardDescription"
+));
+CardDescription.displayName = "CardDescription";
 
-const CardContent = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
-))
-CardContent.displayName = "CardContent"
+type CardContentProps<C extends React.ElementType> = React.PropsWithChildren<
+  AsProp<C>
+> & {
+  ref?: React.ComponentPropsWithRef<C>["ref"];
+  className?: string;
+} & Omit<React.ComponentPropsWithoutRef<C>, "as" | "className">;
+
+type CardContentComponent = (<C extends React.ElementType = "div">(
+  props: CardContentProps<C>
+) => React.ReactElement) & {
+  displayName?: string;
+};
+
+const CardContent: CardContentComponent = React.forwardRef(
+  <C extends React.ElementType = "div">(
+    { as, className, children, ...props }: CardContentProps<C>,
+    ref: React.ComponentPropsWithRef<C>["ref"]
+  ) => {
+    const Component = as || "div";
+    return (
+      <Component ref={ref} className={cn("p-6 pt-0", className)} {...props}>
+        {children}
+      </Component>
+    );
+  }
+) as CardContentComponent;
+
+CardContent.displayName = "CardContent";
 
 const CardFooter = React.forwardRef<
   HTMLDivElement,
@@ -70,7 +116,14 @@ const CardFooter = React.forwardRef<
     className={cn("flex items-center p-6 pt-0", className)}
     {...props}
   />
-))
-CardFooter.displayName = "CardFooter"
+));
+CardFooter.displayName = "CardFooter";
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+};
